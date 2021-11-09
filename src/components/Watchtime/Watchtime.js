2c1d13e4edb47ad5bc7288ce545fcde6da184d68
Rahul Soni <rahulsoni2225@gmail.com>
2021-11-09 22:35:42 +0530

    update: watchtime for shows and movies

--- src/components/Watchtime/Watchtime.js
+++ src/components/Watchtime/Watchtime.js
@@ -1,7 +1,36 @@
+import { GlobalContext } from 'context';
+import { useContext } from 'react';
+
 export const Watchtime = () => {
+  const { watched } = useContext(GlobalContext);
+  //   const [movieTime, setMovieTime] = useState(0);
+  //   const [showTime, setShowTime] = useState(0);
+
+  let totalWatchTimeMovies = watched
+    .map(item => (item.runtime ? item.runtime : 0))
+    .reduce((prev, current) => prev + current);
+  // setMovieTime(totalWatchTimeMovies);
+  let totalWatchTimeShow = watched
+    .map(item =>
+      item.episode_run_time * item.number_of_seasons * item.number_of_episodes
+        ? item.episode_run_time *
+          item.number_of_seasons *
+          item.number_of_episodes
+        : 0,
+    )
+    .reduce((prev, curr) => prev + curr);
+  // setShowTime(totalWatchTimeShow);
+
   return (
-    <div>
-      <h1>Watchtime</h1>
+    <div className="container">
+      {watched.length > 0 ? (
+        <div>
+          <h2>Total Watch Time Movies: {totalWatchTimeMovies}</h2>
+          <h2>Total Watch Time Shows: {totalWatchTimeShow}</h2>
+        </div>
+      ) : (
+        <h2 className="no-movies">No movies in your list! Add some!</h2>
+      )}
     </div>
   );
 };
