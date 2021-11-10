642527cb0b848be507b87a8861109067f7e7b57e
Rahul Soni <rahulsoni2225@gmail.com>
2021-11-10 22:01:33 +0530

    update: fix config url

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -4,10 +4,10 @@ const SEARCH_MOVIE_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&languag
 const SEARCH_TV_BASE_URL = `${API_URL}search/tv?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=`;
 const TRENDING_MOVIE_BASE_URL = `${API_URL}trending/movie/week?api_key=${API_KEY}&language=en-US`;
 const TRENDING_TV_BASE_URL = `${API_URL}trending/tv/week?api_key=${API_KEY}&language=en-US`;
-const POPULAR_MOVIE_BASE_URL = `${API_URL}movie/popular/?api_key=${API_KEY}`;
-const POPULAR_TV_BASE_URL = `${API_URL}tv/popular/?api_key=${API_KEY}&language=en-US`;
-const TOPRATED_MOVIE_BASE_URL = `${API_URL}movie/top_rated/?api_key=${API_KEY}&language=en-US`;
-const TOPRATED_TV_BASE_URL = `${API_URL}tv/top_rated/?api_key=${API_KEY}&language=en-US`;
+const POPULAR_MOVIE_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`;
+const POPULAR_TV_BASE_URL = `${API_URL}tv/popular?api_key=${API_KEY}&language=en-US`;
+const TOPRATED_MOVIE_BASE_URL = `${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US`;
+const TOPRATED_TV_BASE_URL = `${API_URL}tv/top_rated?api_key=${API_KEY}&language=en-US`;
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 const BACKDROP_SIZE = 'w1280';
 const POSTER_SIZE = 'w500';
