8e80036f6323ceaee4b8e77ac20186f28053dd53
Rahul Soni <rahulsoni2225@gmail.com>
2021-11-10 16:32:14 +0530

    update: watchlist

--- src/components/Watchlist/Watchlist.js
+++ src/components/Watchlist/Watchlist.js
@@ -1,18 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { GlobalContext } from 'context';
 import { ItemCard } from 'components';
 import { Link } from 'react-router-dom';
 export const Watchlist = () => {
   const { watchlist } = useContext(GlobalContext);
+  const [shows, setShows] = useState(0);
+  const [movies, setMovies] = useState(0);
 
+  useEffect(() => {
+    const totalMovies = watchlist.filter(item => item.release_date);
+    const totalShows = watchlist.filter(item => item.first_air_date);
+    setShows(totalShows.length);
+    setMovies(totalMovies.length);
+    // eslint-disable-next-line
+  }, []);
+  console.log(shows, movies);
   return (
     <div className="movie-page">
       <div className="container">
         <div className="header">
           <h1 className="heading">My Watchlist</h1>
-
           <span className="count-pill">
-            {watchlist.length} {watchlist.length === 1 ? 'Movie' : 'Movies'}
+            <span className="count-pill">
+              {movies > 0 ? (
+                movies > 1 ? (
+                  movies + ' Movies '
+                ) : (
+                  movies + ' Movie '
+                )
+              ) : (
+                <></>
+              )}{' '}
+              {shows > 0 ? (
+                shows > 1 ? (
+                  shows + ' Shows'
+                ) : (
+                  shows + ' Show'
+                )
+              ) : (
+                <></>
+              )}
+            </span>
           </span>
         </div>
 
