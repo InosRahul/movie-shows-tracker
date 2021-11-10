b3d6b0ae1006f789c413d76f751afe07e75fc7e8
Rahul Soni <rahulsoni2225@gmail.com>
2021-11-10 16:32:30 +0530

    added: watchtime

--- src/components/Watchtime/Watchtime.js
+++ src/components/Watchtime/Watchtime.js
@@ -1,36 +1,66 @@
 import { GlobalContext } from 'context';
 import { useContext } from 'react';
-
+import { conversions } from 'helpers';
 export const Watchtime = () => {
   const { watched } = useContext(GlobalContext);
-  //   const [movieTime, setMovieTime] = useState(0);
-  //   const [showTime, setShowTime] = useState(0);
-
+  console.log(watched);
   let totalWatchTimeMovies = watched
     .map(item => (item.runtime ? item.runtime : 0))
     .reduce((prev, current) => prev + current);
-  // setMovieTime(totalWatchTimeMovies);
   let totalWatchTimeShow = watched
+    .filter(item => item.episode_run_time)
     .map(item =>
-      item.episode_run_time * item.number_of_seasons * item.number_of_episodes
-        ? item.episode_run_time *
+      item.episode_run_time.find(x => x !== undefined) *
+      item.number_of_seasons *
+      item.number_of_episodes
+        ? item.episode_run_time.find(x => x !== undefined) *
           item.number_of_seasons *
           item.number_of_episodes
         : 0,
     )
     .reduce((prev, curr) => prev + curr);
-  // setShowTime(totalWatchTimeShow);
 
+  //   console.log(
+  //     watched
+  //       .filter(item => item.episode_run_time)
+  //       .map(item => item.episode_run_time.find(x => x !== undefined)),
+  //   );
+  let totalWatchTime = totalWatchTimeShow + totalWatchTimeMovies;
   return (
     <div className="container">
-      {watched.length > 0 ? (
-        <div>
-          <h2>Total Watch Time Movies: {totalWatchTimeMovies}</h2>
-          <h2>Total Watch Time Shows: {totalWatchTimeShow}</h2>
-        </div>
-      ) : (
-        <h2 className="no-movies">No movies in your list! Add some!</h2>
-      )}
+      <div className="watchtime">
+        {totalWatchTimeMovies > 0 ? (
+          <h2>
+            Total Watch Time Movies:{' '}
+            <button className="btn">
+              {conversions.timeFormat(totalWatchTimeMovies)}
+            </button>
+          </h2>
+        ) : (
+          <></>
+        )}
+
+        {totalWatchTimeShow > 0 ? (
+          <h2>
+            Total Watch Time Shows:{' '}
+            <button className="btn">
+              {conversions.timeFormat(totalWatchTimeShow)}
+            </button>
+          </h2>
+        ) : (
+          <></>
+        )}
+        {totalWatchTime > 0 ? (
+          <h2>
+            Total Watch Time :
+            <button className="btn">
+              {conversions.timeFormat(totalWatchTime)}
+            </button>
+          </h2>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
