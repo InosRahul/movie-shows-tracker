75b352f59d281ddbd81cf7ece6b7eaf4ab2102cf
Rahul Soni <rahulsoni2225@gmail.com>
2021-11-10 16:32:05 +0530

    update: watched

--- src/components/Watched/Watched.js
+++ src/components/Watched/Watched.js
@@ -1,19 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { GlobalContext } from 'context';
 import { ItemCard } from 'components';
 import { Link } from 'react-router-dom';
 
 export const Watched = () => {
   const { watched } = useContext(GlobalContext);
-
+  const [shows, setShows] = useState(0);
+  const [movies, setMovies] = useState(0);
+  useEffect(() => {
+    const totalMovies = watched.filter(item => item.release_date);
+    const totalShows = watched.filter(item => item.first_air_date);
+    setShows(totalShows.length);
+    setMovies(totalMovies.length);
+    // eslint-disable-next-line
+  }, []);
+  console.log(movies, shows);
   return (
     <div className="movie-page">
       <div className="container">
         <div className="header">
-          <h1 className="heading">Watched Movies</h1>
+          <h1 className="heading">Watched Movies and Shows</h1>
 
           <span className="count-pill">
-            {watched.length} {watched.length === 1 ? 'Movie' : 'Movies'}
+            {movies > 0 ? (
+              movies > 1 ? (
+                movies + ' Movies,'
+              ) : (
+                movies + ' Movie,'
+              )
+            ) : (
+              <></>
+            )}{' '}
+            {shows > 0 ? shows > 1 ? shows + ' Shows' : shows + ' Show' : <></>}
           </span>
         </div>
 
